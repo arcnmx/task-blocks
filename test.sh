496bffb9d7e3a4725809e9f2996e6f2ab65ff8ad
arcnmx <arcnmx@users.noreply.github.com>
2019-04-04 20:12:35 -0700

    simplify onexit hook

--- test.sh
+++ test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -eu
+set -o pipefail
 
 assert() {
 	if ! eval "$1"; then
@@ -16,22 +17,34 @@ export TASKDATA="$__TASK_DATA_DIR"
 export TASKRC="$TASKDATA/taskrc"
 make -e install
 
-touch "$TASKRC"
-task config rc.confirmation=no uda.blocks.type string
-task config rc.confirmation=no uda.blocks.label Blocks
-
 task_() {
 	echo '> task' "$@" >&2
 	command task "$@"
 }
 
-task_ add task1
-task_ add task2 blocks:task1
-assert '[[ $(task +BLOCKING count) -eq 1 ]]'
+touch "$TASKRC"
 
-task_ add task3
-task_ task3 modify blocks:task1
-assert '[[ $(task +BLOCKING count) -eq 2 ]]'
-assert '[[ $(task +BLOCKED count) -eq 1 ]]'
+main() {
+	task config rc.confirmation=no uda.blocks.type string
+	task config rc.confirmation=no uda.blocks.label Blocks
 
-assert '! task_ task3 modify blocks:task4'
+	task_ add task1
+	task_ add task2 blocks:task1
+	assert '[[ $(task +BLOCKING count) -eq 1 ]]'
+
+	task_ add task3
+	task_ task3 modify blocks:task1
+	assert '[[ $(task +BLOCKING count) -eq 2 ]]'
+	assert '[[ $(task +BLOCKED count) -eq 1 ]]'
+
+	assert '! task_ task3 modify blocks:task4'
+
+	assert '[[ $(task blocks.any: count) -eq 0 ]]'
+
+	task_ task3 modify blocks:-task1
+	task_ task2 modify blocks:-task1
+	assert '[[ $(task +BLOCKING count) -eq 0 ]]'
+	assert '[[ $(task +BLOCKED count) -eq 0 ]]'
+}
+
+main 2>&1 | grep -vF " override"
